e1425beb64362a68839130c4dad6c88415304ad9
Utitbest <utitbesta@gmail.com>
2026-07-08 17:48:55 +0100

    Cookies fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -215,8 +215,8 @@ export function login(req, res, next) {
         res
           .cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
           })
           .status(200)
